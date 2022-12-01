fa6d726c4654da90be8aedd18e9a2a0f42972de4
yichi170 <ilee900620@gmail.com>
2022-12-01 17:08:20 +0800

    benchmark

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+#![feature(test)]
+
+extern crate test;
+pub mod pfft;
+pub mod sfft;
+pub mod utils;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+    use hound;
+
+    #[test]
+    fn test_fft() {
+        let mut reader = hound::WavReader::open("audio/sine.wav").unwrap();
+        let samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
+
+        let mut complex_data = utils::initialize(&samples);
+
+        sfft::fft(&mut complex_data);
+        sfft::ifft(&mut complex_data);
+    
+        let new_samples: Vec<i16> = complex_data.iter().map(|c| c.re as i16).collect();
+        
+        let tolerance = -1..2;
+        for i in 0..samples.len() {
+            let diff = samples[i] - new_samples[i];
+            assert!(tolerance.contains(&diff));
+        }
+    }
+
+    #[bench]
+    fn bench_fft(b: &mut Bencher) {
+        b.iter(|| test_fft());
+    }
+}
