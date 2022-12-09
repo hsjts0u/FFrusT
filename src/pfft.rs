01bc576f14868e638137ac1846c7206e2ad9d052
psychicalcoder <psychicalcoder@gmail.com>
2022-12-09 23:59:32 +0800

    split rayon from simd_rfft

--- src/pfft.rs
+++ src/pfft.rs
@@ -46,7 +46,7 @@ fn comp_mult_im(a_re: f64, a_im: f64, b_re: f64, b_im: f64) -> f64 {
     return (a_re * b_im) + (a_im * b_re);
 }
 
-fn simd_rfft(data_re: &mut Vec<f64>, data_im: &mut Vec<f64>, inv: bool, rayon: bool) {
+fn simd_rfft(data_re: &mut Vec<f64>, data_im: &mut Vec<f64>, inv: bool) {
     if data_re.len() != data_im.len() {
         return;
     }
@@ -58,48 +58,101 @@ fn simd_rfft(data_re: &mut Vec<f64>, data_im: &mut Vec<f64>, inv: bool, rayon: b
 
     let sign = if inv { 1.0 } else { -1.0 };
 
-    let mut a0_re = Vec::with_capacity(n / 2);
-    let mut a0_im = Vec::with_capacity(n / 2);
-    let mut a1_re = Vec::with_capacity(n / 2);
-    let mut a1_im = Vec::with_capacity(n / 2);
+    let mut a0_re: Vec<f64> = vec![0.0; n / 2];
+    let mut a0_im: Vec<f64> = vec![0.0; n / 2];
+    let mut a1_re: Vec<f64> = vec![0.0; n / 2];
+    let mut a1_im: Vec<f64> = vec![0.0; n / 2];
 
     for i in 0..n / 2 {
-        a0_re.push(data_re[2 * i]);
-        a0_im.push(data_im[2 * i]);
-        a1_re.push(data_re[2 * i + 1]);
-        a1_im.push(data_im[2 * i + 1]);
+        a0_re[i] = data_re[2 * i];
+        a0_im[i] = data_im[2 * i];
+        a1_re[i] = data_re[2 * i + 1];
+        a1_im[i] = data_im[2 * i + 1];
     }
 
-    if rayon {
-        rayon::join(
-            || simd_rfft(&mut a0_re, &mut a0_im, inv, true),
-            || simd_rfft(&mut a1_re, &mut a1_im, inv, true),
-        );
-    } else {
-        simd_rfft(&mut a0_re, &mut a0_im, inv, false);
-        simd_rfft(&mut a1_re, &mut a1_im, inv, false)
-    }
+    simd_rfft(&mut a0_re, &mut a0_im, inv);
+    simd_rfft(&mut a1_re, &mut a1_im, inv);
 
-    let mut w_re = 1.0;
-    let mut w_im = 0.0;
-    let wn_re = f64::cos(sign * 2.0 * PI / n as f64);
-    let wn_im = f64::sin(sign * 2.0 * PI / n as f64);
+    let wn = Complex {
+        re: f64::cos(sign * 2.0 * PI / n as f64),
+        im: f64::sin(sign * 2.0 * PI / n as f64),
+    };
+
+    let mut warr: Vec<Complex<f64>> = Vec::new();
+    warr.resize(n / 2, Complex::default());
+
+    warr[0] = Complex { re: 1.0, im: 0.0 };
+    for i in 1..n / 2 {
+        warr[i] = warr[i - 1] * wn;
+    }
 
     for i in 0..n / 2 {
-        data_re[i] = a0_re[i] + comp_mult_re(w_re, w_im, a1_re[i], a1_im[i]);
-        data_im[i] = a0_im[i] + comp_mult_im(w_re, w_im, a1_re[i], a1_im[i]);
-        data_re[i + n / 2] = a0_re[i] - comp_mult_re(w_re, w_im, a1_re[i], a1_im[i]);
-        data_im[i + n / 2] = a0_im[i] - comp_mult_im(w_re, w_im, a1_re[i], a1_im[i]);
+        data_re[i] = a0_re[i] + comp_mult_re(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
+        data_im[i] = a0_im[i] + comp_mult_im(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
+        data_re[i + n / 2] = a0_re[i] - comp_mult_re(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
+        data_im[i + n / 2] = a0_im[i] - comp_mult_im(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
         if inv {
             data_re[i] /= 2.0;
             data_im[i] /= 2.0;
             data_re[i + n / 2] /= 2.0;
             data_im[i + n / 2] /= 2.0;
         }
-        let wt_re = w_re;
-        let wt_im = w_im;
-        w_re = comp_mult_re(wt_re, wt_im, wn_re, wn_im);
-        w_im = comp_mult_im(wt_re, wt_im, wn_re, wn_im);
+    }
+}
+
+fn rayon_simd_rfft(data_re: &mut Vec<f64>, data_im: &mut Vec<f64>, inv: bool) {
+    if data_re.len() != data_im.len() {
+        return;
+    }
+
+    let n = data_re.len();
+    if n == 1 {
+        return;
+    }
+
+    let sign = if inv { 1.0 } else { -1.0 };
+
+    let mut a0_re: Vec<f64> = vec![0.0; n / 2];
+    let mut a0_im: Vec<f64> = vec![0.0; n / 2];
+    let mut a1_re: Vec<f64> = vec![0.0; n / 2];
+    let mut a1_im: Vec<f64> = vec![0.0; n / 2];
+
+    for i in 0..n / 2 {
+        a0_re[i] = data_re[2 * i];
+        a0_im[i] = data_im[2 * i];
+        a1_re[i] = data_re[2 * i + 1];
+        a1_im[i] = data_im[2 * i + 1];
+    }
+
+    rayon::join(
+        || rayon_simd_rfft(&mut a0_re, &mut a0_im, inv),
+        || rayon_simd_rfft(&mut a1_re, &mut a1_im, inv),
+    );
+
+    let wn = Complex {
+        re: f64::cos(sign * 2.0 * PI / n as f64),
+        im: f64::sin(sign * 2.0 * PI / n as f64),
+    };
+
+    let mut warr: Vec<Complex<f64>> = Vec::new();
+    warr.resize(n / 2, Complex::default());
+
+    warr[0] = Complex { re: 1.0, im: 0.0 };
+    for i in 1..n / 2 {
+        warr[i] = warr[i - 1] * wn;
+    }
+
+    for i in 0..n / 2 {
+        data_re[i] = a0_re[i] + comp_mult_re(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
+        data_im[i] = a0_im[i] + comp_mult_im(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
+        data_re[i + n / 2] = a0_re[i] - comp_mult_re(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
+        data_im[i + n / 2] = a0_im[i] - comp_mult_im(warr[i].re, warr[i].im, a1_re[i], a1_im[i]);
+        if inv {
+            data_re[i] /= 2.0;
+            data_im[i] /= 2.0;
+            data_re[i + n / 2] /= 2.0;
+            data_im[i + n / 2] /= 2.0;
+        }
     }
 }
 
@@ -114,23 +167,23 @@ pub fn rayon_ifft(complex_data: &mut Vec<Complex<f64>>) {
 pub fn simd_fft(complex_data: &mut Vec<Complex<f64>>) {
     let mut data_re: Vec<f64> = complex_data.into_iter().map(|x| x.re).collect();
     let mut data_im: Vec<f64> = complex_data.into_iter().map(|x| x.im).collect();
-    simd_rfft(&mut data_re, &mut data_im, false, false);
+    simd_rfft(&mut data_re, &mut data_im, false);
 }
 
 pub fn simd_ifft(complex_data: &mut Vec<Complex<f64>>) {
     let mut data_re: Vec<f64> = complex_data.into_iter().map(|x| x.re).collect();
     let mut data_im: Vec<f64> = complex_data.into_iter().map(|x| x.im).collect();
-    simd_rfft(&mut data_re, &mut data_im, true, false);
+    simd_rfft(&mut data_re, &mut data_im, true);
 }
 
 pub fn rayon_simd_fft(complex_data: &mut Vec<Complex<f64>>) {
     let mut data_re: Vec<f64> = complex_data.into_iter().map(|x| x.re).collect();
     let mut data_im: Vec<f64> = complex_data.into_iter().map(|x| x.im).collect();
-    simd_rfft(&mut data_re, &mut data_im, false, true);
+    rayon_simd_rfft(&mut data_re, &mut data_im, false);
 }
 
 pub fn rayon_simd_ifft(complex_data: &mut Vec<Complex<f64>>) {
     let mut data_re: Vec<f64> = complex_data.into_iter().map(|x| x.re).collect();
     let mut data_im: Vec<f64> = complex_data.into_iter().map(|x| x.im).collect();
-    simd_rfft(&mut data_re, &mut data_im, true, true);
+    rayon_simd_rfft(&mut data_re, &mut data_im, true);
 }
