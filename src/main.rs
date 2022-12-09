89ceeee97c192bb551c12a527599eca1619d5bd2
yichi170 <ilee900620@gmail.com>
2022-12-10 00:04:24 +0800

    bench mark macro

--- src/main.rs
+++ src/main.rs
@@ -5,42 +5,51 @@ mod utils;
 
 use std::time::Instant;
 
+macro_rules! bench_fft {
+    ($fft: stmt, $ifft: stmt, $value: expr, $ver: expr) => {
+        print!("{} Elapsed Time: ", $ver);
+
+        for _ in 0..$value {
+            let now = Instant::now();
+            $fft
+            $ifft
+            let elapsed = now.elapsed();
+            print!("{:.2?} ", elapsed);
+        }
+        println!();
+    };
+}
+
 fn main() {
     let mut reader = hound::WavReader::open("audio/rr.wav").unwrap();
     let samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
 
     let mut complex_data = utils::initialize(&samples);
-    let now = Instant::now();
-    sfft::fft(&mut complex_data);
-    sfft::ifft(&mut complex_data);
-    let elapsed = now.elapsed();
-    println!("Serial Elapsed: {:.2?}", elapsed);
+    let bench_niter = 2;
+    bench_fft!(
+        sfft::fft(&mut complex_data),
+        sfft::ifft(&mut complex_data),
+        bench_niter, "Serial"
+    );
 
     let mut complex_data = utils::initialize(&samples);
-    let now = Instant::now();
-    rfft::fft(&mut complex_data);
-    rfft::ifft(&mut complex_data);
-    let elapsed = now.elapsed();
-    println!("Recursive Elapsed: {:.2?}", elapsed);
+    bench_fft!(
+        rfft::fft(&mut complex_data),
+        rfft::ifft(&mut complex_data),
+        bench_niter, "Recursive"
+    );
 
     let mut complex_data = utils::initialize(&samples);
-    let now = Instant::now();
-    pfft::rayon_fft(&mut complex_data);
-    pfft::rayon_ifft(&mut complex_data);
-    let elapsed = now.elapsed();
-    println!("Rayon Elapsed: {:.2?}", elapsed);
+    bench_fft!(
+        pfft::rayon_fft(&mut complex_data),
+        pfft::rayon_ifft(&mut complex_data),
+        bench_niter, "Rayon"
+    );
 
     let mut complex_data = utils::initialize(&samples);
-    let now = Instant::now();
-    pfft::simd_fft(&mut complex_data);
-    pfft::simd_ifft(&mut complex_data);
-    let elapsed = now.elapsed();
-    println!("SIMD Elapsed: {:.2?}", elapsed);
-
-    let mut complex_data = utils::initialize(&samples);
-    let now = Instant::now();
-    pfft::rayon_simd_fft(&mut complex_data);
-    pfft::rayon_simd_ifft(&mut complex_data);
-    let elapsed = now.elapsed();
-    println!("Rayon SIMD Elapsed: {:.2?}", elapsed);
+    bench_fft!(
+        pfft::rayon_simd_fft(&mut complex_data),
+        pfft::rayon_simd_ifft(&mut complex_data),
+        bench_niter, "Rayon SIMD"
+    );
 }
