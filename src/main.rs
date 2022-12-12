7a26c0817aee1ce139b3321df02f268215574889
yichi170 <ilee900620@gmail.com>
2022-12-12 23:21:37 +0800

    feat: run RustFFT

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod rfft;
 mod sfft;
 mod utils;
 
+use rustfft::FftPlanner;
 use std::time::Instant;
 
 macro_rules! bench_fft {
@@ -24,7 +25,7 @@ macro_rules! bench_fft {
 fn main() {
     let mut reader = hound::WavReader::open("audio/rr.wav").unwrap();
     let samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
-    let bench_niter = 1;
+    let bench_niter = 2;
 
     let mut complex_data = utils::initialize(&samples);
     bench_fft!(
@@ -64,5 +65,17 @@ fn main() {
         intrinfft::rayon_simd_ifft(&mut complex_data),
         bench_niter,
         "Rayon SIMD"
+    );
+
+    let mut planner = FftPlanner::<f64>::new();
+    let mut complex_data = utils::initialize(&samples);
+    let len = complex_data.len();
+    let fft = planner.plan_fft_forward(len);
+    let ifft = planner.plan_fft_inverse(len);
+    bench_fft!(
+        fft.process(&mut complex_data),
+        ifft.process(&mut complex_data),
+        bench_niter,
+        "RustFFT"
     );
 }
